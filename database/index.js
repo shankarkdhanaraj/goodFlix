80540402de3e2a115f0408b91c939b36ec17f568
jongregtom <jonthomas83@gmail.com>
2018-07-11 13:12:00 -0700

    (feat) added mongoose database

---
+++ database/index.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const PORT = process.env.PORT || 3000;
+const mongoUrl = `mongodb://localhost/${PORT}`; //check, could be possible error
+
+mongoose.connect(mongoUrl, { useMongoClient: true });
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function() { 
+  console.log('mongo connected')
+})
+
+module.exports = db;
