fb0b7f2b75ea7dd6d16004e207eb157ec54eaaca
Shankar <shankarkdhanaraj@gmail.com>
2018-07-11 22:24:02 -0400

    (feat) home page basic with react - again

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,27 @@ var SRC_DIR = path.join(__dirname, '/client/src');
 var DIST_DIR = path.join(__dirname, '/client/dist');
 
 module.exports = {
-  mode: 'development',
-  // entry: '.client/src/index.jsx',
+  // mode: 'development',
+  entry: './client/src/index.jsx',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: DIST_DIR,
     filename: 'bundle.js'
+  },
+  module : {
+
+    loaders : [
+      {
+        test : /\.jsx?/,
+        include : SRC_DIR,
+        loader : 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      },
+      {
+        test: /\.css?$/,
+        loaders: [ 'style-loader', 'css-loader' ]
+      }
+    ]
   }
 };
