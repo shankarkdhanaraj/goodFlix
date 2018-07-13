8a7db5d59f34ca59eec89dae4944617f6bca491d
jongregtom <jonthomas83@gmail.com>
2018-07-13 16:30:44 -0700

    modified Title component, added logo

--- client/src/components/Title.jsx
+++ client/src/components/Title.jsx
@@ -1,29 +1,20 @@
 import React from 'react';
+import {Header, Image} from 'semantic-ui-react';
 
 const Title = function(props) {
   if (props.isLoggedIn !== true) {
-  return (<div>
-    <span>    	
-    <img src="https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjilbTaqJrcAhUorVQKHY1rAO0QjRx6BAgBEAU&url=http%3A%2F%2Fwww.way2ads.co%2Fcoloring-worksheet%2Fmovie-reel-drawing.html&psig=AOvVaw13yOyrh61aYMjwfmz9vVGb&ust=1531510812967632" className="logo"/>
-    </span>  
-    <span>  
-    <h1 className="title" >GoodFlix</h1>
-    </span>
-    <span> 
-    <h4 className="subtitle" >Find your next movie</h4>
-    </span>  
+  return (<div> 
+    <Header as='h1' width='50px'>
+      <Image src="reel.png" size='large'/>
+      GoodFlix ...find your next movie
+    </Header>
   </div>)
   } else {
   	return (<div>
-  	<span>  	
-    <img src="https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjilbTaqJrcAhUorVQKHY1rAO0QjRx6BAgBEAU&url=http%3A%2F%2Fwww.way2ads.co%2Fcoloring-worksheet%2Fmovie-reel-drawing.html&psig=AOvVaw13yOyrh61aYMjwfmz9vVGb&ust=1531510812967632" className="logo"/>
-    </span> 
-    <span>   
-    <h1 className="title">Welcome</h1>
-    </span> 
-    <span>   
-    <h4 className="subtitle">{props.userName}</h4>
-    </span>  
+    <Header as='h1'>
+      <Image src="reel.png" size='large'/>
+      Welcome, {props.userName}
+    </Header> 
   </div>)
   }
 }
