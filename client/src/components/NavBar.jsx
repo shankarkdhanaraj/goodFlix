fcd9a4416e76ecfdd3f0c1c194da0592758fae96
Pooja <pooja.subramanian3@gmail.com>
2018-07-12 19:09:48 -0700

    adding NavBar component(partially responsive)

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import { Input, Menu } from 'semantic-ui-react'
+
+export default class MenuExampleSecondary extends Component {
+  constructor(){
+    super()
+  this.state = { activeItem: 'home' }
+  this.handleItemClick = this.handleItemClick.bind(this);
+}
+
+  handleItemClick(e, { name }) {
+    this.setState({ activeItem: name })
+  }
+
+  render() {
+    const { activeItem } = this.state
+
+    return (
+      <Menu secondary>
+        <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
+        <Menu.Item
+          name='Watchers'
+          active={activeItem === 'Watchers'}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Item
+          name='My Movies'
+          active={activeItem === 'My Movies'}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Menu position='right'>
+          <Menu.Item>
+            <Input icon='search' placeholder='Search for your favorite movies..' />
+          </Menu.Item>
+          <Menu.Item
+            name='logout'
+            active={activeItem === 'logout'}
+            onClick={this.handleItemClick}
+          />
+        </Menu.Menu>
+      </Menu>
+    )
+  }
+}
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Button } from 'semantic-ui-react';
+import NavBar from './components/NavBar.jsx';
 import 'semantic-ui-css/semantic.min.css';
 
 class App extends React.Component {
@@ -14,6 +15,7 @@ render() {
 return (
   <div>
     <h1>GoodFlix</h1>
+    <NavBar/>
     <h4>The only way to find your next movie!!!</h4>
     <Button primary>Primary</Button>
     <h6>Brought to you by hello-world of RPT07</h6>
