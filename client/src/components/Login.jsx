56944a172b8c6ab602707b3204d7f836be5eacc3
Shankar <shankarkdhanaraj@gmail.com>
2018-07-16 02:10:06 -0400

    (feat) login to signup and vice versa

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
-import { Grid } from 'semantic-ui-react';
+import { Grid, Label } from 'semantic-ui-react';
 import UsernamePassword from './UsernamePassword.jsx';
 
 export default class Login extends React.Component {
   constructor(props) {
     super(props);
+    this.login = this.login.bind(this);
+    this.gotoSignup = this.gotoSignup.bind(this);
+  }
+
+  login(userPassword) {
+    alert(`Logging...`);
+  }
+
+  gotoSignup() {
+    this.props.changeToSignup();
   }
 
   render() {
@@ -12,9 +22,19 @@ export default class Login extends React.Component {
     return (
       <Grid>
         <Grid.Row>
-        <Grid.Column>
-          <UsernamePassword/>
-        </Grid.Column>
+          <Grid.Column>
+            <h3>Login</h3>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column>
+            <UsernamePassword onSubmit={this.login}/>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column>
+            <div>No account? <Label as='a' basic onClick={ this.gotoSignup }> Sign up </Label></div>
+          </Grid.Column>
         </Grid.Row>
       </Grid>
     );
