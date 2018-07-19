fe6867e68a0ebc7a90473b38756243f6c65079ae
Shankar <shankarkdhanaraj@gmail.com>
2018-07-19 02:17:46 -0400

    db connection update for heroku

--- database/index.js
+++ database/index.js
@@ -6,8 +6,15 @@ const PORT = process.env.PORT;// || 3000;
 //const mongoUrl = `mongodb://dbGoodFlix:helloworld1@ds133601.mlab.com:33601/heroku_pn2wqhdh`
 // console.log(`mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_SERVER}`);
 // const mongoUrl = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_SERVER}`;
-const mongoUrl = `mongodb://${process.env.DB_SERVER}`;
 
+var mongoUrl
+if ( process.env.NODE_ENV === 'production' ) {
+  mongoUrl = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_SERVER}`;
+} else {
+  mongoUrl = `mongodb://${process.env.DB_SERVER}`;
+}
+
+console.log('mongoUrl is...', mongoUrl);
 mongoose.connect(mongoUrl, { useNewUrlParser: true });
 
 const db = mongoose.connection;
