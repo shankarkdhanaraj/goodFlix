c7c4059fbde6da50f04aae8b9e9dd58a837f4ca1
shankarkdhanaraj <shankarkdhanaraj@gmail.com>
2018-07-19 02:00:33 -0400

    connect to heroku mongodb with user pswd

--- database/index.js
+++ database/index.js
@@ -6,7 +6,12 @@ const PORT = process.env.PORT;// || 3000;
 //const mongoUrl = `mongodb://dbGoodFlix:helloworld1@ds133601.mlab.com:33601/heroku_pn2wqhdh`
 // console.log(`mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_SERVER}`);
 // const mongoUrl = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_SERVER}`;
-const mongoUrl = `mongodb://${process.env.DB_SERVER}`;
+
+if ( process.env.NODE_ENV === 'production' ) {
+  const mongoUrl = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_SERVER}`;
+} else {
+  const mongoUrl = `mongodb://${process.env.DB_SERVER}`;
+}
 
 mongoose.connect(mongoUrl, { useNewUrlParser: true });
 
